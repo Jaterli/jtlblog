2b9bdd6fb442b2fdc3c912ecbdeadbafcc244142
jt <jaterli@hotmail.com>
2025-02-19 12:06:33 +0100

    VITE_PUBLIC_ACCESS_KEY

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_KEY: string;
+    readonly VITE_PUBLIC_ACCESS_KEY: string;
+    readonly VITE_BASE_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
