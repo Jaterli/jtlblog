997de680580358b549e7d0b98e613e7fae44524c
jt <jaterli@hotmail.com>
2025-02-22 23:39:24 +0100

    netlify form24

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -11,7 +11,7 @@ const handleSubmit = (e) => {
       <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         <strong class="font-bold">Error!</strong>
         <span class="block sm:inline">Por favor, completa el reCAPTCHA.</span>
-        <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" data-bs-dismiss="alert" aria-label="Close">
+        <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none'" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
@@ -30,7 +30,7 @@ const handleSubmit = (e) => {
           <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
             <strong class="font-bold">Error!</strong>
             <span class="block sm:inline">Parece que hubo un problema con el reCAPTCHA. Por favor, inténtalo de nuevo.</span>
-            <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" data-bs-dismiss="alert" aria-label="Close">
+            <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none'" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
@@ -41,21 +41,22 @@ const handleSubmit = (e) => {
           <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
             <strong class="font-bold">¡Gracias!</strong>
             <span class="block sm:inline">Tu mensaje ha sido enviado.</span>
-            <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" data-bs-dismiss="alert" aria-label="Close">
+            <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none'" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
         `;
         contactForm.reset();
-        contactForm.style.display = "none"; // Oculta el formulario
+        //contactForm.style.display = "none"; // Oculta el formulario
       }
     })
     .catch((error) => {
+      console.error('Error:', error);
       submitMessage.innerHTML = `
         <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
           <strong class="font-bold">Error!</strong>
           <span class="block sm:inline">No se pudo enviar el mensaje, por favor inténtalo de nuevo más tarde.</span>
-          <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" data-bs-dismiss="alert" aria-label="Close">
+          <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none'" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
