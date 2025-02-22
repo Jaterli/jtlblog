c5a88c3a2c94ed8c8d1abb6211f0798336a419fc
jt <jaterli@hotmail.com>
2025-02-23 00:14:15 +0100

    netlify form26

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -8,12 +8,12 @@ const handleSubmit = (e) => {
 
   if (!formData.get('g-recaptcha-response')) {
     submitMessage.innerHTML = `
-      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative flex items-start" role="alert">
+      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         <div class="flex-1">
-          <strong class="font-bold">Error!</strong>
+          <strong class="font-bold">Error!</strong><br />
           <span class="block sm:inline"> Por favor, completa el reCAPTCHA.</span>
         </div>
-        <button type="button" class="ml-4 text-red-700 hover:text-red-900" onclick="this.parentElement.style.display='none'" aria-label="Close">
+        <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
           &times;
         </button>
       </div>
@@ -29,12 +29,12 @@ const handleSubmit = (e) => {
     .then((response) => {
       if (response.status === 303) {
         submitMessage.innerHTML = `
-          <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative flex items-start" role="alert">
+          <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
             <div class="flex-1">
-              <strong class="font-bold">Error!</strong>
+              <strong class="font-bold">Error!</strong><br />
               <span class="block sm:inline"> Parece que hubo un problema con el reCAPTCHA. Por favor, inténtalo de nuevo.</span>
             </div>
-            <button type="button" class="ml-4 text-red-700 hover:text-red-900" onclick="this.parentElement.style.display='none'" aria-label="Close">
+            <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
               &times;
             </button>
           </div>
@@ -42,12 +42,12 @@ const handleSubmit = (e) => {
         return;
       } else {
         submitMessage.innerHTML = `
-          <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative flex items-start" role="alert">
+          <div class="bg-green-100 border border-red-400 text-green-700 px-4 py-3 rounded relative" role="alert">
             <div class="flex-1">
-              <strong class="font-bold">¡Gracias!</strong>
+              <strong class="font-bold">¡Gracias!</strong><br />
               <span class="block sm:inline"> Tu mensaje ha sido enviado.</span>
             </div>
-            <button type="button" class="ml-4 text-green-700 hover:text-green-900" onclick="this.parentElement.style.display='none'" aria-label="Close">
+            <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
               &times;
             </button>
           </div>
@@ -58,12 +58,12 @@ const handleSubmit = (e) => {
     .catch((error) => {
       console.error('Error:', error);
       submitMessage.innerHTML = `
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative flex items-start" role="alert">
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
           <div class="flex-1">
-            <strong class="font-bold">Error!</strong>
+            <strong class="font-bold">Error!</strong><br />
             <span class="block sm:inline"> No se pudo enviar el mensaje, por favor inténtalo de nuevo más tarde.</span>
           </div>
-          <button type="button" class="ml-4 text-red-700 hover:text-red-900" onclick="this.parentElement.style.display='none'" aria-label="Close">
+          <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
             &times;
           </button>
         </div>
