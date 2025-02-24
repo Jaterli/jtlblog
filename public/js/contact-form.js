e991d3d2b21e99ae93b313c8b95fa76f81bdb190
jt <jaterli@hotmail.com>
2025-02-24 13:21:28 +0100

    netlify form29

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -2,22 +2,18 @@ const handleSubmit = (e) => {
   e.preventDefault();
 
   const contactForm = document.getElementById("contact-form");
-  const submitMessage = document.getElementById("contact-form-result");
+  const msg_error_container = document.getElementById("msg-error-container")
+  const msg_error_text = msg_error_container.querySelector("span");
+  const msg_success_container = document.getElementById("msg-success-container")  
 
   let formData = new FormData(contactForm);
 
   if (!formData.get('g-recaptcha-response')) {
-    submitMessage.innerHTML = `
-      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-        <div class="flex-1">
-          <strong class="font-bold">Error!</strong><br />
-          <span class="block sm:inline"> Por favor, completa el reCAPTCHA.</span>
-        </div>
-        <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
-          &times;
-        </button>
-      </div>
+    msg_error_text.innerHTML = `
+      Por favor, completa el reCAPTCHA
     `;
+    msg_success_container.display='none';
+    msg_error_container.style.display='block';
     return;
   }
 
@@ -28,46 +24,25 @@ const handleSubmit = (e) => {
   })
     .then((response) => {
       if (response.status === 303) {
-        submitMessage.innerHTML = `
-          <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <div class="flex-1">
-              <strong class="font-bold">Error!</strong><br />
-              <span class="block sm:inline"> Parece que hubo un problema con el reCAPTCHA. Por favor, inténtalo de nuevo.</span>
-            </div>
-            <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
-              &times;
-            </button>
-          </div>
+        msg_error_text.innerHTML = `
+          Parece que hubo un problema con el reCAPTCHA. Por favor, inténtalo de nuevo.</span>
         `;
+        msg_success_container.display='none';
+        msg_error_container.style.display='block';
         return;
       } else {
-        submitMessage.innerHTML = `
-          <div class="bg-green-100 border border-red-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-            <div class="flex-1">
-              <strong class="font-bold">¡Gracias!</strong><br />
-              <span class="block sm:inline"> Tu mensaje ha sido enviado.</span>
-            </div>
-            <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
-              &times;
-            </button>
-          </div>
-        `;
+        msg_error_container.style.display='none';                
+        msg_success_container.display='block';        
         contactForm.reset();
       }
     })
     .catch((error) => {
       console.error('Error:', error);
-      submitMessage.innerHTML = `
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-          <div class="flex-1">
-            <strong class="font-bold">Error!</strong><br />
-            <span class="block sm:inline"> No se pudo enviar el mensaje, por favor inténtalo de nuevo más tarde.</span>
-          </div>
-          <button type="button" class="absolute top-0 right-1 px-0 py-0" onclick="this.parentElement.style.display='none'" aria-label="Close">
-            &times;
-          </button>
-        </div>
+      msg_error_text.innerHTML = `
+        No se pudo enviar el mensaje, por favor inténtalo de nuevo más tarde.</span>
       `;
+      msg_success_container.display='none';
+      msg_error_container.style.display='block';      
     });
 };
 
