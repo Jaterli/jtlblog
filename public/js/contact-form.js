8298da5e64a7030b0d97a28a7f83708edd1f60e6
jt <jaterli@hotmail.com>
2025-04-14 19:01:15 +0200

    hide captcha

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -18,37 +18,37 @@ const handleSubmit = (e) => {
     msg_error_container.style.display='block';
     return;
   }else{
-    contactForm.submit();
-  }
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded", "Cache-Control": "no-store" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then((response) => {
-      if (response.status === 303) {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded", "Cache-Control": "no-store" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then((response) => {
+        if (response.status === 303) {
+          msg_error_text.innerHTML = `
+            Parece que hubo un problema con el reCAPTCHA. Por favor, inténtalo de nuevo.</span>
+          `;
+          msg_success_container.style.display='none';
+          msg_error_container.style.display='block';
+          return;
+        } else {
+          msg_error_container.style.display='none';                
+          msg_success_container.style.display='block';        
+          contactForm.reset();
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
         msg_error_text.innerHTML = `
-          Parece que hubo un problema con el reCAPTCHA. Por favor, inténtalo de nuevo.</span>
+          No se pudo enviar el mensaje, por favor inténtalo de nuevo más tarde.</span>
         `;
         msg_success_container.style.display='none';
-        msg_error_container.style.display='block';
-        return;
-      } else {
-        msg_error_container.style.display='none';                
-        msg_success_container.style.display='block';        
-        contactForm.reset();
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      msg_error_text.innerHTML = `
-        No se pudo enviar el mensaje, por favor inténtalo de nuevo más tarde.</span>
-      `;
-      msg_success_container.style.display='none';
-      msg_error_container.style.display='block';      
-    });
-};
+        msg_error_container.style.display='block';      
+      });
+  };
+  
+}
+
 
 const contactForm = document.getElementById("contact-form");
 if (contactForm) {
