d162b882b9f2223b5a106cfd0ea2af5e79ca01ab
jt <jaterli@hotmail.com>
2025-04-14 21:29:46 +0200

    hide captcha

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -1,54 +1,60 @@
-const handleSubmit = (e) => {
-  e.preventDefault();
+// const handleSubmit = (e) => {
+//   e.preventDefault();
   
-  const contactForm = document.getElementById("contact-form");
-  const msg_error_container = document.getElementById("msg-error-container");
-  const msg_error_text = msg_error_container.querySelector("span");
-  const msg_success_container = document.getElementById("msg-success-container");
-  const captcha = document.getElementById("recaptcha-container");
+//   const contactForm = document.getElementById("contact-form");
+//   const msg_error_container = document.getElementById("msg-error-container");
+//   const msg_error_text = msg_error_container.querySelector("span");
+//   const msg_success_container = document.getElementById("msg-success-container");
+//   const captcha = document.getElementById("recaptcha-container");
 
-  let formData = new FormData(contactForm);
+//   let formData = new FormData(contactForm);
 
   // Mostrar recaptcha si no está completado
-  if (!formData.get('g-recaptcha-response')) {
-    captcha.style.display = 'block';
-    msg_error_text.innerHTML = "Por favor, completa el reCAPTCHA";
-    msg_success_container.style.display = 'none';
-    msg_error_container.style.display = 'block';
-    return;
-  }
+  // if (!formData.get('g-recaptcha-response')) {
+  //   captcha.style.display = 'block';
+  //   msg_error_text.innerHTML = "Por favor, completa el reCAPTCHA";
+  //   msg_success_container.style.display = 'none';
+  //   msg_error_container.style.display = 'block';
+  //   return;
+  // }
 
   // Enviar formulario si el recaptcha está completo
-  fetch("/", {
-    method: "POST",
-    headers: { 
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Cache-Control": "no-store" 
-    },
-    body: new URLSearchParams(formData).toString(),
-  })
-  .then((response) => {
-    if (response.status === 303) {
-      throw new Error('Error de reCAPTCHA');
-    }
-    msg_error_container.style.display = 'none';
-    msg_success_container.style.display = 'block';
-    contactForm.reset();
-    captcha.style.display = 'none'; // Ocultar recaptcha después de enviar
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-    msg_error_text.innerHTML = "Error al enviar, intenta nuevamente";
-    msg_success_container.style.display = 'none';
-    msg_error_container.style.display = 'block';
-  });
-}
+//   fetch("/", {
+//     method: "POST",
+//     headers: { 
+//       "Content-Type": "application/x-www-form-urlencoded",
+//       "Cache-Control": "no-store" 
+//     },
+//     body: new URLSearchParams(formData).toString(),
+//   })
+//   .then((response) => {
+//     if (response.status === 303) {
+//       throw new Error('Error de reCAPTCHA');
+//     }
+//     msg_error_container.style.display = 'none';
+//     msg_success_container.style.display = 'block';
+//     contactForm.reset();
+//     captcha.style.display = 'none'; // Ocultar recaptcha después de enviar
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//     msg_error_text.innerHTML = "Error al enviar, intenta nuevamente";
+//     msg_success_container.style.display = 'none';
+//     msg_error_container.style.display = 'block';
+//   });
+// }
 
-// Función global para el callback de recaptcha
-function onCaptchaSuccess() {
-  const contactForm = document.getElementById("contact-form");
-  alert('Enviando formulario')
-  contactForm.dispatchEvent(new Event('submit'));
+// // Función global para el callback de recaptcha
+// function onCaptchaSuccess() {
+//   const contactForm = document.getElementById("contact-form");
+//   alert('Enviando formulario')
+//   contactForm.dispatchEvent(new Event('submit'));
+// }
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  grecaptcha.execute();
 }
 
 // Inicializar el evento submit
